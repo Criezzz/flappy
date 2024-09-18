8cec4abe9f64a3428691996e0671ce8f638515b1
nammm <namtotet205@gmail.com>
2024-09-18 18:01:21 +0700

    almost complete. Lack sounds

--- Assets/script/gameManager.cs
+++ Assets/script/gameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,42 +11,60 @@ public class gameManager : MonoBehaviour
     public GameObject player;
     public GameObject prefab;
     public GameObject cv;
+    public GameObject scoreMsg;
     public bool inGame;
     public bool gameOver;
+    public int score;
+    public GameObject scoreRegular;
 
     private void Awake()
     {
+        score = 0;
         Time.timeScale = 1;
         instance = this;
         inGame = false;
         gameOver = false;
+        scoreRegular = cv.transform.Find("scoreRegular").gameObject;
     }   
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
         if (!inGame)
         {
             if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
             {
                 play();
+                
             }
+        }
+        if (inGame)
+        {
+            scoreRegular.GetComponent<TextMeshProUGUI>().text = score.ToString();
         }
         if (gameOver)
         {
             Time.timeScale = 0f;
-            cv.transform.Find("playButton").gameObject.SetActive( true);
-
+            GameObject gameoverScene = cv.transform.Find("gameover").gameObject;
+            gameoverScene.SetActive( true);
+            string v = score.ToString();
+            scoreMsg.transform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = v;
+            scoreMsg.transform.Find("bestText").GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetInt("best").ToString();
         }
     }
-    private void Spawn() //4->0 y
+    private void Spawn() //3.8->0 y
     {
-        Instantiate(prefab, new Vector3(7.55f, Random.Range(4f,  0f), 0 ), Quaternion.identity);
+        Instantiate(prefab, new Vector3(7.55f, Random.Range(3.8f,  0f), 0 ), Quaternion.identity);
     }
     public void play()
     {
         inGame = true;
         cv.transform.Find("message").gameObject.SetActive(false);
+        scoreRegular.SetActive(true);
         player.GetComponent<playerControl>().enabled = true;
         InvokeRepeating("Spawn", 4f, 0.8f);
     }
-    
+
+
 }
--- Assets/script/pipeScroll.cs
+++ Assets/script/pipeScroll.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class pipeScroll : MonoBehaviour
 {
     public float scrollingSpeed = 5.3f; // to match ground speed
+    private bool crossed;
     // Update is called once per frame
+    private void Awake()
+    {
+        crossed = false;
+    }
     void Update()
     {
+        if (transform.position.x <= gameManager.instance.player.transform.position.x-2 && crossed == false)
+        {
+            gameManager.instance.score += 1 ;
+            if(gameManager.instance.score > PlayerPrefs.GetInt("best"))
+            {
+                PlayerPrefs.SetInt("best", gameManager.instance.score);
+            }
+            crossed = true;
+        }
+        if(transform.position.x < -11.6f)
+        {
+            Destroy(gameObject);
+        }
         transform.position -= new Vector3(scrollingSpeed * Time.deltaTime, 0 , 0 );
     }
 }
